e46db13138190bc2f06029f1d0ee35811ecc8bfb
Zalak-P <zalakjpatel14@gmail.com>
2026-04-27 21:46:50 -0700

    Create 2_MaximumNumberOfVowelsInSubstringOfGivenLength.py

---
+++ Sliding_Window/3_FIXED_WINDOW/2_MaximumNumberOfVowelsInSubstringOfGivenLength.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        hashmap = {'a', 'e', 'i', 'o', 'u'}
+        left = right = counter = result = 0
+
+        while right < len(s):
+            if s[right] in hashmap:
+                counter += 1
+
+            while right - left + 1 > k:
+                if s[left] in hashmap:
+                    counter -= 1
+                left += 1
+            
+            result = max(result, counter)
+            right += 1
+            
+        return result
