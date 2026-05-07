c0913b88a9d91423882a18f9375943749d39d9a2
Zalak-P <zalakjpatel14@gmail.com>
2026-05-07 00:39:40 -0700

    Create 2_Min_Stack.py

---
+++ Practice_Questions/Stacks_Queues/1_Basic/2_Min_Stack.py
@@ -0,0 +1,36 @@
+# Problem: https://leetcode.com/problems/min-stack/
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        if not self.min_stack: #first element push
+            self.min_stack.append(val)
+        else:
+            if val <= self.min_stack[-1]:
+                self.min_stack.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            element = self.stack.pop()
+            #to avoid popping the min val, first check
+            if self.min_stack and element == self.min_stack[-1]: 
+                self.min_stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+        
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
