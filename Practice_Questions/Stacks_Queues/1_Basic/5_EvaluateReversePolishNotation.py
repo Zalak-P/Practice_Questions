c06fd61bd021de362120b904f94d05ab23d376ba
Zalak-P <zalakjpatel14@gmail.com>
2026-05-07 01:33:10 -0700

    Update 5_EvaluateReversePolishNotation.py

--- Practice_Questions/Stacks_Queues/1_Basic/5_EvaluateReversePolishNotation.py
+++ Practice_Questions/Stacks_Queues/1_Basic/5_EvaluateReversePolishNotation.py
@@ -1,4 +1,5 @@
 # Problem: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+# Core Trick: If token is number, push it. If token is operator, pop num2 then num1, calculate num1 operator num2, and push result back.
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
