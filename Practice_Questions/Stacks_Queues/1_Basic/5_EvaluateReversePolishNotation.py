effa1b2babf01b31cdf7f4e2d42e029a1e192701
Zalak-P <zalakjpatel14@gmail.com>
2026-05-07 01:31:48 -0700

    Create 5_EvaluateReversePolishNotation.py

---
+++ Practice_Questions/Stacks_Queues/1_Basic/5_EvaluateReversePolishNotation.py
@@ -0,0 +1,27 @@
+# Problem: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for item in tokens:
+            if item not in "+-/*":
+                stack.append(int(item)) #bit integer cannot be 
+                continue
+
+            num2 = stack.pop()
+            num1 = stack.pop()
+            ans = 0 
+            
+            if item == '+':
+                ans = num1 + num2
+            elif item == '-':
+                ans = num1 - num2
+            elif item == '*':
+                ans = num1 * num2
+            else:
+                ans = int(num1 / num2) #bit integer cannot be 
+            
+            stack.append(ans)
+
+        return stack.pop()
