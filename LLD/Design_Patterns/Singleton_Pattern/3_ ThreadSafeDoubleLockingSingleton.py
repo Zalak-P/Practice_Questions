ef2f66768a1927871bb179aebb825bbf375eabc7
Zalak-P <zalakjpatel14@gmail.com>
2026-05-13 21:16:27 -0700

    Update 3_ ThreadSafeDoubleLockingSingleton.py

--- LLD/Design_Patterns/Singleton_Pattern/3_ ThreadSafeDoubleLockingSingleton.py
+++ LLD/Design_Patterns/Singleton_Pattern/3_ ThreadSafeDoubleLockingSingleton.py
@@ -5,25 +5,8 @@ class ThreadSafeDoubleLockingSingleton:
     _lock = threading.Lock()
 
     def __new__(cls):
-        # -----------------------------------------
-        # First Check
-        # No locking yet.
-        # Avoids unnecessary locking after Singleton already exists.
-        # -----------------------------------------
-
         if cls._instance is None:
-            # -----------------------------------------
-            # Acquire Lock
-            # Only ONE thread can enter here.
-            # -----------------------------------------
-
             with cls._lock:
-                # -----------------------------------------
-                # Second Check
-                # Another thread may have already
-                # created object while current thread
-                # was waiting for lock.
-                # -----------------------------------------
                 if cls._instance is None:
                     print("Creating Singleton Object...")
                     cls._instance = super().__new__(cls)
