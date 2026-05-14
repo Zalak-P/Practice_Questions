7929a95b7295a3c6e6980bde6ac1aedc01f9bd59
Zalak-P <zalakjpatel14@gmail.com>
2026-05-13 21:57:57 -0700

    Update 1_NoSingleton.py

--- LLD/Design_Patterns/Singleton_Pattern/1_NoSingleton.py
+++ LLD/Design_Patterns/Singleton_Pattern/1_NoSingleton.py
@@ -1,16 +1,35 @@
 class NoSingleton:
+
     def __init__(self):
-    # Runs AFTER object is created.
-    # Purpose: initialize object. NOT create object. Very important.
-    print("New Object created.")
+        print("New Object created.")
+
 
 print("----- No Singleton Example -----")
-
-# s1 -> Object Creation, Python internally does __new__() creates object in memory.
 s1 = NoSingleton()
-# SAME happens again. Another brand NEW object created.
 s2 = NoSingleton()
-
 print("s1", s1)
-print("s2", s2) # this will return two different objects
-print("s1 is s2 :", s1 is s2) # is checks Memory identity: Output -> False
+print("s2", s2)
+print("s1 is s2 :", s1 is s2)
+
+
+# 1. __init__()
+# Runs after object creation.
+# Used for object initialization.
+
+# 2. s1 = NoSingleton()
+# Python internally calls:
+# - __new__() -> creates object
+# - __init__() -> initializes object
+
+# 3. s2 = NoSingleton()
+# Another completely new object gets created.
+
+# 4. s1 is s2
+# 'is' checks whether both variables point to the SAME object.
+
+# Output:
+# False
+
+# because:
+# - s1 points to Object A
+# - s2 points to Object B
