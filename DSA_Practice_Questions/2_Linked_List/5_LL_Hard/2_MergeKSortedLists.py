3357e96887f65f9a77dd8234371291a714848fa8
Zalak-P <zalakjpatel14@gmail.com>
2026-05-14 22:48:50 -0700

    Create 2_MergeKSortedLists.py

---
+++ DSA_Practice_Questions/2_Linked_List/5_LL_Hard/2_MergeKSortedLists.py
@@ -0,0 +1,29 @@
+# Problem: https://leetcode.com/problems/merge-k-sorted-lists/
+# Trick: Put each list head in min-heap. Always pop smallest node, attach it in result list, then push its next node.
+
+from typing import List, Optional
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        minHeap = []
+
+        # Put first node of every list into heap
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(minHeap, (node.val, i, node))
+
+        dummy = ListNode(0)
+        temp = dummy
+
+        while minHeap:
+            val, i, node = heapq.heappop(minHeap)
+
+            temp.next = node
+            temp = temp.next
+
+            if node.next:
+                heapq.heappush(minHeap, (node.next.val, i, node.next))
+
+        return dummy.next
