cb130c07881e5a170ec41b8ce529e376427ada6b
Zalak-P <zalakjpatel14@gmail.com>
2026-05-16 18:01:48 -0700

    Create Decorator_Pattern.py

---
+++ LLD/Design_Patterns/6_DecoratorPattern/Decorator_Pattern.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    @abstractmethod
+    def get_abilities(self):
+        pass
+
+class Mario(Character):
+    def get_abilities(self):
+        return "Mario"
+
+class CharacterDecorator(Character):
+    def __init__(self, character):
+        self.character = character
+
+class HeightUp(CharacterDecorator):
+    def get_abilities(self):
+        return self.character.get_abilities() + " with HeightUp"
+
+class GunPowerUp(CharacterDecorator):
+    def get_abilities(self):
+        return self.character.get_abilities() + " with Gun"
+
+class StarPowerUp(CharacterDecorator):
+    def get_abilities(self):
+        return self.character.get_abilities() + " with Star Power (Limited Time)"
+
+mario = Mario()
+print("Basic Character:", mario.get_abilities())
+
+mario = HeightUp(mario)
+print("After HeightUp:", mario.get_abilities())
+
+mario = GunPowerUp(mario)
+print("After GunPowerUp:", mario.get_abilities())
+
+mario = StarPowerUp(mario)
+print("After StarPowerUp:", mario.get_abilities())
