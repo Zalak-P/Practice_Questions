924489155d48eb87de01d1909351d04a6521f191
Zalak-P <zalakjpatel14@gmail.com>
2026-07-23 23:18:03 -0700

    Update 1_Valid_Parentheses.py

--- DSA_Practice_Questions/3_Stacks_Queues/1_Basic/1_Valid_Parentheses.py
+++ DSA_Practice_Questions/3_Stacks_Queues/1_Basic/1_Valid_Parentheses.py
@@ -4,14 +4,13 @@ class Solution:
     def isValid(self, s: str) -> bool:
         hashmap = {')': '(', '}':'{', ']':'['}
         stack = []
+        
         for char in s:
-            if char not in hashmap:
-                stack.append(char)
-            else:
-                if stack:
-                    open_ = stack.pop()
-                    if open_ != hashmap[char]:
-                        return False
-                else:
+            if char in hashmap:
+                _open = stack.pop() if stack else '#'
+                if hashmap[char] != _open:
                     return False
-        return True if not stack else False
+            else:
+                stack.append(char)
+                
+        return not stack
