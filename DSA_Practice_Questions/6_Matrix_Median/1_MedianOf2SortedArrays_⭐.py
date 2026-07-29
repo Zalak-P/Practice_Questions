331b6acdfea37552169e16d82e443f368f4db946
Zalak-P <zalakjpatel14@gmail.com>
2026-07-28 17:58:15 -0700

    Update 1_MedianOf2SortedArrays_⭐.py

--- DSA_Practice_Questions/6_Matrix_Median/1_MedianOf2SortedArrays_⭐.py
+++ DSA_Practice_Questions/6_Matrix_Median/1_MedianOf2SortedArrays_⭐.py
@@ -1,4 +1,4 @@
-# Core Trick: You shift your partition to left or right in nums1.
+# Core Trick: You start in the middle of A and shift your partition to left or right in nums1.
 
 # Aleft   | Aright
 # ---------|---------
